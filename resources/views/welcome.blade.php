542feaec7bb91bae38bf700c49f820ee3488c3da
Diego Moreno <diegomoreno@cyandark.com>
2017-08-25 11:05:36 -0500

    Vista welcome, link PlayStore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
             </div>
             <div class="col-lg-8 col-md-8 col-sm-8 img_banner">
                 <img src="/img/Info.png" alt="IES" class="img-responsive" width="762px" height="295px" style="padding-top: 40px;">
-                <a><img class="margin-playstore playstore" href="https://play.google.com/store/apps/details?id=com.unisangil.ies" target="__blank" src="/img/playstore.png" alt="PlayStore" class="img-responsive" width="236px" height="100%" border="none"></a>
+                <a href="https://play.google.com/store/apps/details?id=com.unisangil.ies" target="__blank"><img class="margin-playstore playstore" src="/img/playstore.png" alt="PlayStore" class="img-responsive" width="236px" height="100%" border="none"></a>
             </div>
         </div>
     </div>
